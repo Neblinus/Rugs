231e9b0bce28255a71851bb78a3a768f8b9f45bc
Davi Ricieri <102916102+Neblinus@users.noreply.github.com>
2026-04-06 20:09:52 -0300

    Complete Color implementation

--- rugs.rb
+++ rugs.rb
@@ -1,18 +1,19 @@
-# Raylib Wrapper for Ruby
+# Raylib wrapper for Ruby
 require "ffi"
 
-module Rawby
-	# Color class definition for FFI wrapping
-  require_relative "raw_color"
-  # Basic original library setup
+### FFI wrappings for Raylib structs
+require_relative "Structs/colors"
+
+# The library wrapping itself
+module Rugs
 	extend FFI::Library
 	ffi_lib "raylib"
 	# Define the wrapper functions with aliasing for Ruby conventions
 	attach_function :init_window, :InitWindow, [:int, :int, :string], :void
-	attach_function :window_should_close, :WindowShouldClose, [], :bool
+	attach_function :window_should_close?, :WindowShouldClose, [], :bool
 	attach_function :begin_drawing, :BeginDrawing, [], :void
-	attach_function :clear_background, :ClearBackground, [Color], :void
-	attach_function :draw_text, :DrawText, [:string, :int, :int, :int, Color], :void
+	attach_function :clear_background, :ClearBackground, [Color.by_value], :void
+	attach_function :draw_text, :DrawText, [:string, :int, :int, :int, Color.by_value], :void
 	attach_function :end_drawing, :EndDrawing, [], :void
 	attach_function :set_target_fps, :SetTargetFPS, [:int], :void
 end
