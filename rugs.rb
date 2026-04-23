1df8e0c5cc5a6b1fd8398b8a91a9aced20da0832
Davi Ricieri <102916102+Neblinus@users.noreply.github.com>
2026-04-23 17:30:53 -0300

    Add Rectangle struct, fix Color struct file name

--- rugs.rb
+++ rugs.rb
@@ -2,11 +2,12 @@
 require "ffi"
 
 ### FFI wrappings for Raylib structs
-require_relative "Structs/colors"
+require_relative "Structs/color"
 require_relative "Structs/vector_2"
 require_relative "Structs/vector_3"
 require_relative "Structs/vector_4"
 require_relative "Structs/matrix"
+require_relative "Structs/rectangle"
 
 # The library wrapping itself
 module Rugs
@@ -17,7 +18,8 @@ module Rugs
 	attach_function :window_should_close?, :WindowShouldClose, [], :bool
 	attach_function :begin_drawing, :BeginDrawing, [], :void
 	attach_function :clear_background, :ClearBackground, [Color.by_value], :void
-	attach_function :draw_text, :DrawText, [:string, :int, :int, :int, Color.by_value], :void
+	attach_function :draw_text, :DrawText, [:string, :int, :int, :int, Color.by_value],
+	:void
 	attach_function :end_drawing, :EndDrawing, [], :void
 	attach_function :set_target_fps, :SetTargetFPS, [:int], :void
 end
