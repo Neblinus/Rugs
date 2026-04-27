c31603ec18cbdb22dd6bcc0ea39936832b40f43d
Davi Ricieri <102916102+Neblinus@users.noreply.github.com>
2026-04-27 18:36:28 -0300

    Final addition before testing of pre-calling processing

--- Enums/enums.rb
+++ Enums/enums.rb
@@ -1,8 +1,10 @@
 # Group the Raylib enums together
 require_relative "config_flags"
 require_relative "trace_log_level"
+require_relative "keyboard_key"
 
 module Enums
 	include ConfigFlags
 	include TraceLogLevel
+	include KeyboardKey
 end
--- Enums/trace_log_level.rb
+++ Enums/trace_log_level.rb
@@ -1,12 +1,28 @@
 # Define the Raylib TraceLogLevel enum (as a module with constants)
+require "ffi"
 
 module TraceLogLevel
-	LOG_ALL = 0
-  LOG_TRACE = 1
-  LOG_DEBUG = 2
-  LOG_INFO = 3
-  LOG_WARNING = 4
-  LOG_ERROR = 5
-  LOG_FATAL = 6
-  LOG_NONE = -1
+  # Basic FFI setup
+  extend FFI::Library
+  ffi_lib "raylib"
+  # Define the enum constants
+  enum :TraceLogLevel,
+  [
+    LOG_ALL = 0,
+    LOG_TRACE = 1,
+    LOG_DEBUG = 2,
+    LOG_INFO = 3,
+    LOG_WARNING = 4,
+    LOG_ERROR = 5,
+    LOG_FATAL = 6,
+    LOG_NONE = -1
+  ]
+  # Allows use of the enums as virtual constants
+  def TraceLogLevel.const_missing(target_symbol)
+    # Use FFI specifics to get the constant value, or raise an exception on error
+    value = enum_value(target_symbol)
+    return super unless value
+    # On success, return the constant value
+    value
+  end
 end
